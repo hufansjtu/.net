b6be2d3d0f360cdcea701c1ccd12154529da2b6e
16321 <hufan@csvw.com>
2022-01-24 16:18:22 +0800

    添加项目文件。

---
+++ App_Code/dbhelp.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// dbhelp 的摘要说明
+/// </summary>
+public class dbhelp
+{
+    public static string constr = @"server=10.122.37.185";
+    public dbhelp()
+    {
+        //
+        // TODO: 在此处添加构造函数逻辑
+        //
+    }
+    public static DataSet getds(string sql) {
+        DataSet ds = new DataSet();
+        SqlConnection con = new SqlConnection(constr);
+        SqlCommand cmd = new SqlCommand(sql, con);
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        try
+        {
+            con.Open();
+            da.Fill(ds);
+            con.Close();
+            con.Dispose();//注销
+            cmd.Dispose();
+            da.Dispose();
+        }
+        catch
+        {
+            con.Close();
+            con.Dispose();//注销
+            cmd.Dispose();
+            da.Dispose();
+        }
+        return ds;
+    }
+    public static DataTable getdt(string sql)
+    {
+        DataTable dt = new DataTable();
+        SqlConnection con = new SqlConnection(constr);
+        SqlCommand cmd = new SqlCommand(sql, con);
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        try
+        {
+            con.Open();
+            da.Fill(dt);
+            con.Close();
+            con.Dispose();//注销
+            cmd.Dispose();
+            da.Dispose();
+        }
+        catch
+        {
+            con.Close();
+            con.Dispose();//注销
+            cmd.Dispose();
+            da.Dispose();
+        }
+        return dt;
+    }
+    public static bool sacesql(string sql)
+    {
+        bool ifsuccess = false;
+        SqlConnection con = new SqlConnection(constr);
+        SqlCommand cmd = new SqlCommand(sql, con);
+        try
+        {
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            con.Dispose();
+            cmd.Dispose();
+        }
+        catch
+        {
+            con.Close();
+            con.Dispose();
+            cmd.Dispose();
+        }
+        return ifsuccess;
+    }
+    public static void alert(string mesg)
+    {
+        HttpContext.Current.Response.Write("<script type=\"text/javascript\">alert('"+mesg+"'):</script>");
+    }
+
+}
---
+++ index.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="index.aspx.cs" Inherits="index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+    <frameset rows="120,*" frameborder="no" border="0">
+        <frame name="top" src="top.aspx" border="0" scrolling="no">
+
+        </frame>
+    </frameset>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
